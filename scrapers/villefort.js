3ef8a2e50889c6cceeb7909ef6515ef6c4848f3d
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-11 11:27:59 -0300

    modularize login token generating function

--- scrapers/villefort.js
+++ scrapers/villefort.js
@@ -1,46 +1,11 @@
 import fetch from 'node-fetch';
+import getVillefortToken from '../utils/getVillefortToken.js';
 
 let cachedToken = null;
 
-async function getVillefortToken() {
-	if (cachedToken) return cachedToken;
-
-	try {
-		const response = await fetch(
-			'https://services.vipcommerce.com.br/api-admin/v1/auth/loja/login',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					Origin: 'https://www.villefortentrega.com.br',
-					Referer: 'https://www.villefortentrega.com.br/',
-					organizationid: '120',
-				},
-				body: JSON.stringify({
-					domain: 'villefortentrega.com.br',
-					key: 'df072f85df9bf7dd71b6811c34bdbaa4f219d98775b56cff9dfa5f8ca1bf8469',
-					username: 'loja',
-				}),
-			}
-		);
-
-		const res = await response.json();
-
-		if (!res.success) {
-			throw new Error(`Login failed: ${res.message || 'Unknown error'}`);
-		}
-
-		cachedToken = res.data;
-		return cachedToken;
-	} catch (err) {
-		console.error('Failed to get Villefort token:', err.message);
-		return null;
-	}
-}
-
 async function getProductDetails(productId) {
 	if (!cachedToken) {
-		await getVillefortToken();
+		cachedToken = await getVillefortToken();
 	}
 
 	let response = await fetch(
@@ -60,9 +25,7 @@ async function getProductDetails(productId) {
 	);
 
 	if (response.status === 403) {
-		cachedToken = null;
-		await getVillefortToken();
-
+		cachedToken = await getVillefortToken();
 		return getProductDetails(productId);
 	}
 
---
+++ utils/getVillefortToken.js
@@ -0,0 +1,35 @@
+async function getVillefortToken() {
+	try {
+		const response = await fetch(
+			'https://services.vipcommerce.com.br/api-admin/v1/auth/loja/login',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Origin: 'https://www.villefortentrega.com.br',
+					Referer: 'https://www.villefortentrega.com.br/',
+					organizationid: '120',
+				},
+				body: JSON.stringify({
+					domain: 'villefortentrega.com.br',
+					key: 'df072f85df9bf7dd71b6811c34bdbaa4f219d98775b56cff9dfa5f8ca1bf8469',
+					username: 'loja',
+				}),
+			}
+		);
+
+		const res = await response.json();
+
+		if (!res.success) {
+			throw new Error(`Login failed: ${res.message || 'Unknown error'}`);
+		}
+
+		const cachedToken = res.data;
+		return cachedToken;
+	} catch (err) {
+		console.error('Failed to get Villefort token:', err.message);
+		return null;
+	}
+}
+
+export default getVillefortToken;
