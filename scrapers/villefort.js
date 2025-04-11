97c4fc19185357c2c1dba7673f734308a4c43533
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-11 13:36:00 -0300

    modularize villefort getPrice function

--- scrapers/villefort.js
+++ scrapers/villefort.js
@@ -1,35 +1,41 @@
 import fetch from 'node-fetch';
 import getVillefortToken from '../utils/getVillefortToken.js';
 
-async function getProductDetails(productId) {
+async function getPrice(productArray) {
 	let token = await getVillefortToken();
+	const results = [];
 
-	let response = await fetch(
-		`https://services.vipcommerce.com.br/api-admin/v1/loja/produtos/${productId}/filial/1/centro_distribuicao/1/detalhes`,
-		{
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${token}`,
-				authority: 'services.vipcommerce.com.br',
-				'Content-Type': 'application/json',
-				Origin: 'https://www.villefortentrega.com.br',
-				Referer: 'https://www.villefortentrega.com.br/',
-				organizationid: '120',
-				Domainkey: 'villefortentrega.com.br',
-			},
+	for (const product of productArray) {
+		let response = await fetch(
+			`https://services.vipcommerce.com.br/api-admin/v1/loja/produtos/${product.sku}/filial/1/centro_distribuicao/1/detalhes`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${token}`,
+					authority: 'services.vipcommerce.com.br',
+					'Content-Type': 'application/json',
+					Origin: 'https://www.villefortentrega.com.br',
+					Referer: 'https://www.villefortentrega.com.br/',
+					organizationid: '120',
+					Domainkey: 'villefortentrega.com.br',
+				},
+			}
+		);
+
+		if (response.status === 403) {
+			token = await getVillefortToken();
+			return getPrice(productArray);
 		}
-	);
 
-	if (response.status === 403) {
-		token = await getVillefortToken();
-		return getProductDetails(productId);
+		const res = await response.json();
+		results.push({
+			Brand: product.brand,
+			Product: res.data.produto.descricao,
+			Price: res.data.produto.preco,
+			Market: product.market,
+		});
 	}
-
-	const res = await response.json();
-	return {
-		Product: res.data.produto.descricao,
-		Price: res.data.produto.preco,
-	};
+	return results;
 }
 
-console.log(await getProductDetails(5401));
+export default getPrice;
