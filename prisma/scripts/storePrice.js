283c2c40f29a4508f792e8ab52c406670818dd4e
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-21 10:21:30 -0300

    only update prices when a change is detected

--- prisma/scripts/storePrice.js
+++ prisma/scripts/storePrice.js
@@ -1,18 +1,33 @@
 import prisma from '../client.js';
+import retrievePriceHistory from './retrievePriceHistory.js';
 const today = new Date();
 today.setHours(0, 0, 0, 0);
 
 async function storePrice(productDataArray) {
 	try {
+		//check latest price first
 		for (const productData of productDataArray) {
-			await prisma.price.create({
-				data: {
-					productId: productData.id,
-					price: productData.Price,
-					date: today,
-				},
-			});
-			console.log(`Stored price for ${productData.id} on database.`);
+			const history = await retrievePriceHistory(
+				null,
+				null,
+				null,
+				productData.id
+			);
+
+			const latestPrice = history[0].prices[history[0].prices.length - 1].price;
+			//only store price if a price change is detected
+			if (latestPrice != productData.Price || history.length === 0) {
+				await prisma.price.create({
+					data: {
+						productId: productData.id,
+						price: productData.Price,
+						date: today,
+					},
+				});
+				console.log(`Stored price for ${productData.id} on database.`);
+			} else {
+				console.log(`No price change for ${productData.id}.`);
+			}
 		}
 	} catch (error) {
 		console.error(error);
