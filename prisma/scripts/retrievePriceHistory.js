3858ef10388fd288ba44ca113af869e78af4bb0b
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-21 10:21:08 -0300

    add function to retrieve prices from database

---
+++ prisma/scripts/retrievePriceHistory.js
@@ -0,0 +1,33 @@
+import prisma from '../client.js';
+
+async function retrievePriceHistory(type, brand, market, id) {
+	try {
+		const filters = {};
+		if (type) filters.type = type;
+		if (brand) filters.brand = brand;
+		if (market) filters.market = market;
+		if (id) filters.id = id;
+		console.log(`Getting price history for provided filters.`);
+
+		const priceHistory = await prisma.product.findMany({
+			where: filters,
+			select: {
+				id: true,
+				name: true,
+				market: true,
+				prices: {
+					select: {
+						price: true,
+						date: true,
+					},
+				},
+			},
+		});
+
+		return priceHistory;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export default retrievePriceHistory;
---
+++ utils/priceHistory.js
@@ -0,0 +1,18 @@
+import retrievePriceHistory from '../prisma/scripts/retrievePriceHistory.js';
+
+async function priceHistory(...args) {
+	const history = await retrievePriceHistory(...args);
+
+	if (history.length === 0) {
+		console.log('No price history found for provided filters.');
+	}
+
+	for (const item of history) {
+		console.log(item.name);
+		console.log(item);
+	}
+
+	return history;
+}
+
+priceHistory('feijao');
