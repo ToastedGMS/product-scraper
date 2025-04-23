c68b45425b1d8b66e110d801903cc631dc0c5474
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-23 14:05:18 -0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+import cors from 'cors';
+
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	})
+);
+app.options('*', cors());
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
