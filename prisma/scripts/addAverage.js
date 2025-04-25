21133c8b0c2a63388a2c42699157fae66c9c8b99
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-25 09:32:36 -0300

    fix: update average price calculation to use rounded value and improve existing price check

--- prisma/scripts/addAverage.js
+++ prisma/scripts/addAverage.js
@@ -1,6 +1,6 @@
 import prisma from '../client.js';
 const today = new Date();
-today.setHours(12, 0, 0, 0); //normalize time to avoid duplicate prices being stored for the same day
+today.setHours(12, 0, 0, 0); // Normalize time to avoid duplicate prices being stored for the same day
 
 async function addAveragePrice(type, priceArray) {
 	if (priceArray.length === 0) {
@@ -9,14 +9,35 @@ async function addAveragePrice(type, priceArray) {
 	}
 	const averagePrice =
 		priceArray.reduce((a, b) => a + b, 0) / priceArray.length;
+
+	const roundedPrice = Math.round(averagePrice * 100) / 100;
+
 	try {
+		// Check if the average price for the product type already exists for today
+		const existingPrice = await prisma.averageDailyPrice.findUnique({
+			where: {
+				productType_date: {
+					productType: type,
+					date: today,
+				},
+			},
+		});
+
+		if (existingPrice) {
+			console.log(
+				`Average price for ${type} already exists for today, skipping...`
+			);
+			return;
+		}
 		const result = await prisma.averageDailyPrice.create({
 			data: {
 				productType: type,
-				price: averagePrice,
+				price: roundedPrice,
 				date: today,
 			},
 		});
+
+		console.log(`Stored average price for ${type}: ${roundedPrice}`);
 	} catch (error) {
 		console.error(error);
 		throw error;
